5e7b36d296ea249656b2244b2ed0a3b41e9e4e1a
Leegaeun1 <igaeun013@gmail.com>
2025-11-07 15:54:35 +0900

    ocr edit

--- upload/urls.py
+++ upload/urls.py
@@ -6,6 +6,7 @@ URL과 views.py 안의 함수를 연결하는 역할'''
 from django.urls import path
 from . import views
 
+# 클라이언트가 접근하는 경로(엔드포인트) : "","health/"... -> views.XXX와 연결되어 XXX함수가 실행됨
 urlpatterns = [
     path("health/", views.health, name="health"),
     path("", views.upload_form, name="upload_form"),
